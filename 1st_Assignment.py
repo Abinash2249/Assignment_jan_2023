8c239f11f337891294865dc5582a14a6369008f2
Abinash2249 <abinashdahal2001@gmail.com>
2024-01-28 16:29:45 +0545

    Loop added

--- 1st_Assignment.py
+++ 1st_Assignment.py
@@ -34,9 +34,12 @@ print(result2)
 # d2 = {'a': 300, 'b': 200, 'd':400}
 # output: {'a': 400, 'b': 400, 'd': 400, 'c': 300}
 
+d1 = {"a": 100, "b": 200, "c": 300}
+d2 = {"a": 300, "b": 200, "c": 400}
 
-
-
+result3 = d1.values()
+result4 = d2.values()
+result5 = result3 + result4
 
 
 
